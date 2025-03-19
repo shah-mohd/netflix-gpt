41cf86d06d5e8d1b15efc675769b6cd31d3114d5
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-03-19 11:22:09 +0530

    validate, redux, auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,72 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValidData } from '../utils/validate';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+
 
 const Login = () => {
 
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const email = useRef(null);
   const password = useRef(null);
+  const name = useRef(null);
 
   const handleButtonClick = () => {
     // validate the form data
     const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
+    if(message) return;
+
+    // sign up and sign in logic
+    if(!isSignInForm){
+      // sign up logic
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+      .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/121492725?v=4"
+      }).then(() => {
+        // Profile updated!
+        const {uid, email, displayName, photoURL} = auth.currentUser;
+        dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
+        navigate("/browse");
+      }).catch((error) => {
+        // An error occurred
+        setErrorMessage(error.message);
+      });
+
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      setErrorMessage(errorCode + " " + errorMessage);
+    });
+    }
+    else {
+      // sign in ligic
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate("/browse");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode + "-" + errorMessage);
+      });
+    }
   }
 
   const toggeleSignInForm = ()=> {
@@ -46,6 +99,7 @@ const Login = () => {
             </h1>
 
             {!isSignInForm && <input
+            ref={name}
             className='my-4 p-4 w-full bg-gray-700'
             type='text'
             placeholder='Full Name'
